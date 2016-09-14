817943b48f17e9b9a2797df90feae150f15e679b
camark <camark@live.cn>
2016-09-14 08:43:42 +0800

    init

---
+++ freeeim_package.h
@@ -0,0 +1,15 @@
+#ifndef _FreeEIM_Package
+#define _FreeEIM_Package
+// Download by http://www.codefans.net
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+class FreeEIM_Package
+{
+public:
+	FreeEIM_Package();
+	~FreeEIM_Package();
+};
+#endif // _FreeEIM_Package
